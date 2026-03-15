c97198c0a4d2e96761461bf90821a8e8003656e2
unknown <daniel.deguzman025@gmail.com>
2026-03-16 02:37:58 +0800

    cange api

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      "/api": "http://localhost:3001"
+      "/api": "https://notionnik-backend.onrender.com"
     }
   }
 })
